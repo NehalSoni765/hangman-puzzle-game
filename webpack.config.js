1a77991618a790f49b16681e58b5b6605585f9d1
NehalSoni765 <nebakso16@gmail.com>
2023-10-26 21:50:21 +0530

    Updated package json and gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-module.exports={
-    entry: ['babel-polyfill','./src/index.js'],//start process
-    output:{
-        filename:'bundle.js',
-        path: path.resolve(__dirname,'public/scripts')
+module.exports = {
+  entry: ["babel-polyfill", "./src/index.js"], //start process
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/scripts"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //before .js and afterfor $
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: path.join(__dirname, "public/"),
+    devMiddleware: {
+      publicPath: "/scripts/",
     },
-    module:{
-        rules:[{
-            test:/\.js$/, //before .js and afterfor $
-            exclude:/node_modules/,
-            use:{
-                loader:"babel-loader",
-                options:{
-                    presets:['@babel/preset-env']
-                }
-            }
-        }]
-    },
-    devServer:{
-        contentBase: path.resolve(__dirname,"public"),
-        publicPath: '/scripts/'
-    },
-    devtool:'source-map' // it converts babel code to our code in browser devtool
-}
+    port: 3002,
+  },
+  devtool: "source-map", // it converts babel code to our code in browser devtool
+};
